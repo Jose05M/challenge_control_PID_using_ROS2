3bf817746130592c590822e1fb03271779ca5c3b
Jóse Eduardo Sánchez Martínez <146748476+Jose05M@users.noreply.github.com>
2026-03-11 09:31:03 -0600

    Add files via upload

---
+++ mcr2_challenge_final/mcr2_challenge_final.ino
@@ -0,0 +1,294 @@
+// Include Libraries to be used
+#include <micro_ros_arduino.h>    //micro-ros-arduino library
+#include <rcl/rcl.h>              //Core ROS 2 Client Library (RCL) for node management.
+#include <rcl/error_handling.h>   //Error handling utilities for Micro-ROS.
+#include <rclc/rclc.h>            //Micro-ROS Client library for embedded devices.
+#include <rclc/executor.h>        //Micro-ROS Executor to manage callbacks
+#include <std_msgs/msg/float32.h>   //Predefined ROS 2 message type
+#include <rmw_microros/rmw_microros.h> //ROS Middleware for Micro-ROS, provides functions for interfacing Micro-ROS with DDS.
+#include <stdio.h>                //Standard I/O library for debugging.
+#include <std_msgs/msg/float32_multi_array.h>
+
+//Declare nodes to be used
+rcl_node_t node;            //Represents a ROS 2 Node running on the microcontroller.
+
+//Instantiate executor and its support classes
+rclc_executor_t executor;   //Manages task execution (timers, callbacks, etc.).
+rclc_support_t support;     //Data structure that holds the execution context of Micro-ROS, including its communication state, memory management, and initialization data.
+rcl_allocator_t allocator;  //Manages memory allocation.
+
+//Declare Subscribers to be used
+rcl_subscription_t subscriber;
+rcl_publisher_t publisher;
+rcl_publisher_t publisher_vel;
+
+//Declare Messages to be used
+std_msgs__msg__Float32 msg_sub;  //Defines a message of type float32.
+//std_msgs__msg__Float32 msg_pub;
+std_msgs__msg__Float32MultiArray msg_pub;
+std_msgs__msg__Float32 msg_vel;
+
+//Define Macros to be used
+//Executes fn and returns false if it fails.
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
+// Executes fn, but ignores failures.
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+//  Executes a given statement (X) periodically every MS milliseconds
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)\
+
+//Defines State Machine States
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state = WAITING_AGENT;
+
+//Specifies GPIO pin 13 for controlling an LED
+#define PHASEA_GPIO 14
+#define PHASEB_GPIO 13
+#define IN1 25
+#define IN2 26
+#define PWM_PIN 27 //DEFINE PWM_PIN
+
+#define RPM_MAX 134.0        // RPM máximas de salida del gearmotor
+#define PULSES_PER_REV 495   // Resolucion del encoder
+
+#define PWM_FRQ 980 //Define PWM Frequency
+#define PWM_RES 8  //Define PWM Resolution
+#define PWM_MAX        255
+#define PWM_CHNL 0    //Define Channel
+#define MSG_MIN_VAL -1 //Define min input value
+#define MSG_MAX_VAL 1 //Define max input value
+float mag;
+float setPoint_RPM;
+
+//Create entity functions
+bool create_entities();
+void destroy_entities();
+
+//Define callbacks
+void subscription_callback(const void * msgin)
+{  
+  const std_msgs__msg__Float32 * msg = (const std_msgs__msg__Float32 *)msgin;
+  setPoint_RPM = constrain(msg->data, MSG_MIN_VAL, MSG_MAX_VAL);
+  mag = abs(setPoint_RPM);
+}
+
+// Encoder
+volatile long countPhaseA = 0;
+long pulseCount = 0;
+
+// Velocidad
+float rpm_raw = 0.0;
+float rpm = 0.0;
+float rpm_filt = 0.0;
+
+// Referencia
+float setPoint_percent = 0.0;
+//float setPoint_RPM = 0.0;
+// Variable de process variable
+float processVariable_percent = 0.0;
+
+// Control
+int pwm = 0;
+float controlSignal = 0.0; // 0..100%
+
+// PID Incremental
+float Kp = 1.6;
+float Ki = 0.6;
+float Kd = 0.02;
+float e0 = 0.0, e1 = 0.0, e2 = 0.0;
+  
+const float alphaRPM = 0.20; 
+
+// Tiempo de muestreo
+const float Ts = 0.05; // 100 ms
+float clampf(float x, float xmin, float xmax) {
+  if (x < xmin) return xmin;
+  if (x > xmax) return xmax;
+  return x;
+}
+// ISR Encoder
+int dir = 1;
+void IRAM_ATTR isr() {
+  if(digitalRead(PHASEB_GPIO)){
+    dir = 1;
+  }else{
+    dir = -1;
+  }countPhaseA++;
+}
+
+void control_function(){
+  noInterrupts();
+  pulseCount = countPhaseA;
+  countPhaseA = 0;
+  interrupts();
+
+  //RPM 
+  rpm_raw = (pulseCount * 60.0) / (PULSES_PER_REV * Ts);
+  rpm_raw = clampf(rpm_raw,0.0,RPM_MAX);
+  rpm_filt = alphaRPM * rpm_raw + (1.0 -alphaRPM) * rpm_filt;
+  rpm = rpm_filt/RPM_MAX;
+
+  float ref_rpm = mag;
+
+  if (mag < 0.01) {
+    controlSignal = 0;
+    e0 = 0;
+    e1 = 0;
+    e2 = 0;
+  } else {
+    e0 = ref_rpm - rpm;
+  }
+
+  //PID incremental
+  controlSignal = controlSignal
+    + Kp * (e0 -e1)
+    + Ki * Ts * e0
+    + (Kd / Ts) * (e0 -2.0 * e1 + e2);
+  
+  //SATURACION
+  controlSignal = constrain(controlSignal,0,1);
+  //CONVERTIR A PWM
+  uint32_t duty = (uint32_t)(controlSignal * PWM_MAX);
+
+  // Controlar dirección
+  if(setPoint_RPM > 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else if(setPoint_RPM < 0){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  } else{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    duty = 0;
+  }
+  ledcWrite(PWM_CHNL, duty);
+  e2 = e1;
+  e1 = e0;
+  float time_sec = millis()/1000.0;
+  msg_pub.data.data[0] = time_sec;
+  msg_pub.data.data[1] = setPoint_RPM;
+  msg_pub.data.data[2] = controlSignal;
+  msg_pub.data.data[3] = rpm * dir;
+
+  msg_vel.data = rpm * dir;
+  rcl_publish(&publisher_vel, &msg_vel, NULL);
+
+  rcl_publish(&publisher,&msg_pub,NULL);
+}
+
+//Setup
+void setup() {
+  set_microros_transports(); // Initializes communication between ESP32 and the ROS 2 agent (Serial).
+  //Setup Microcontroller Pins
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  
+    // Encoder
+  pinMode(PHASEA_GPIO, INPUT_PULLUP);
+  pinMode(PHASEB_GPIO, INPUT_PULLUP);
+
+  ledcSetup(PWM_CHNL, PWM_FRQ, PWM_RES);  //Setup the PWM
+  ledcAttachPin(PWM_PIN, PWM_CHNL);       //Setup Attach the Pin to the Channel  
+  
+  // Interrupción encoder
+  attachInterrupt(digitalPinToInterrupt(PHASEA_GPIO), isr, FALLING);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  ledcWrite(PWM_CHNL, 0);
+  msg_pub.data.data = (float*) malloc(4*sizeof(float));
+  msg_pub.data.size = 4;
+  msg_pub.data.capacity = 4;
+}
+
+void loop() {
+  switch (state) {
+
+    case WAITING_AGENT:
+      EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+      break;
+
+    case AGENT_AVAILABLE:
+      state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+      if (state == WAITING_AGENT) {
+        destroy_entities();
+      };
+      break;
+
+    case AGENT_CONNECTED:
+      EXECUTE_EVERY_N_MS(50,control_function());
+      EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+      if (state == AGENT_CONNECTED) {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+      }
+      break;
+
+    case AGENT_DISCONNECTED:
+      destroy_entities();
+      state = WAITING_AGENT;
+      break;
+      
+    default:
+      break;
+  }
+}
+
+bool create_entities()
+{
+   //Initializes memory allocation for Micro-ROS operations.
+  allocator = rcl_get_default_allocator();
+
+   //Creates a ROS 2 support structure to manage the execution context.
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "motor_control", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+    "set_point"));
+
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+    "motor_output"));
+  RCCHECK(rclc_publisher_init_default(
+    &publisher_vel,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+    "motor_velocity"));
+
+  // create zero initialised executor (no configured) to avoid memory problems
+  executor = rclc_executor_get_zero_initialized_executor();
+  // Initializes the Micro-ROS Executor, which manages tasks and callbacks.
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  // Register suscription with executor
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg_sub, &subscription_callback, ON_NEW_DATA));
+
+  return true;
+}
+
+void destroy_entities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+  rcl_publisher_fini(&publisher,&node);
+  rcl_publisher_fini(&publisher_vel,&node);
+  rcl_subscription_fini(&subscriber, &node);
+  rclc_executor_fini(&executor);
+  rcl_node_fini(&node);
+  rclc_support_fini(&support);
+}
